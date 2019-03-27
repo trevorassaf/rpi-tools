da07149131d6c9c8d2f904aad44853afbb42f27e
Trevor Assaf <bozkurtus@google.com>
2019-03-27 00:30:36 -0700

    Make rpi-wifi.sh operational
    Also add README

---
+++ host/rpi-wifi/README.md
@@ -0,0 +1,9 @@
+# RPI WIFI
+
+## Attach RPi to wifi network with the followig command
+
+./rpi-wifi.sh --ssid=<ssid> --password=<password> --interface=<interface>
+
+## Example
+
+./rpi-wifi.sh --rpi=pi2 --ssid="ATT9EJZ63d" --password="9si9rp?dt4dj" --interface=wlan0
--- host/rpi-wifi/rpi-wifi.sh
+++ host/rpi-wifi/rpi-wifi.sh
@@ -11,7 +11,7 @@ function error_lecho() {
 }
 
 function usage() {
-  error_lecho "$BINARY_NAME --rpi=<rpi ssh target> --ssid=<network ssid> [--password=<network password>] [--interface=<network interface>]"
+  error_lecho "$BINARY_NAME --rpi=<rpi ssh target> --ssid=<network ssid> --password=<network password> --interface=<network interface>"
   error_lecho "\t-r --rpi       RPi ssh target, e.g. pi2, pi@192.168.200.2"
   error_lecho "\t-s --ssid      SSID for target network"
   error_lecho "\t-p --password  Password for target network"
@@ -43,7 +43,7 @@ while true; do
   esac
 done
 
-if [ -z "$RPI" -o -z "$SSID" ]; then
+if [ -z "$RPI" -o -z "$SSID" -o -z "$PASSWORD" ]; then
   usage
   exit 1
 fi
@@ -62,5 +62,33 @@ if [ -z "$INTERFACE_CHECK" ]; then
   error_lecho "Available interfaces..."
   error_lecho "$IWCONFIG_OUTPUT"
   usage
+  exit 1
+fi
+
+WPA_PASSWORD_OUTPUT=
+WPA_PASSWORD_OUTPUT=$(cat<<EOF
+network={
+  ssid=\""$SSID"\"
+  psk=\""$PASSWORD"\"
+}
+EOF
+)
+
+UPDATE_WPA_CONF_OUTPUT=
+UPDATE_WPA_CONF_OUTPUT=`ssh $RPI "echo \"$WPA_PASSWORD_OUTPUT\" | /usr/bin/sudo /usr/bin/tee -a /etc/wpa_supplicant/wpa_supplicant.conf"`
+UPDATE_WPA_CONF_RESULT=$?
+
+if [ "$UPDATE_WPA_CONF_RESULT" != 0 ]; then
+  error_lecho "Failed to write wpa conf file: $UPDATE_WPA_CONF_RESULT"
+  error_lecho $UPDATE_WPA_CONF_OUTPUT
+  exit 1
+fi
+
+RESET_WIFI_CONFIG_OUTPUT=
+RESET_WIFI_CONFIG_OUTPUT=`ssh $RPI "/sbin/wpa_cli -i $INTERFACE reconfigure"`
+RESET_WIFI_CONFIG_RESULT=$?
+
+if [ "$RESET_WIFI_CONFIG_RESULT" != "0" ]; then
+  error_lecho "Failed to reconfigure wpa_cli: $RESET_WIFI_CONFIG_OUTPUT"
   exit 1
 fi
